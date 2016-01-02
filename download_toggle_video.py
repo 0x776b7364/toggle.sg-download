56f20567554a19f22edc8c18c4d14fa79906b9a0
0x776b7364 <0x776b7364>
2016-01-02 18:07:50 +1300

    Fixed auto-download function
    Auto-download, despite its name, does not perform downloads
    automatically. Created a preference list of formats and auto-queues the
    best quality format for download.

--- download_toggle_video.py
+++ download_toggle_video.py
@@ -8,6 +8,7 @@ import os
 import time
 import sys
 import argparse
+import Queue
 
 # set to 1 for debugging
 # note that enabling debugging writes files to disk
@@ -30,6 +31,13 @@ checkAndDlSubs = 1
 
 VALID_URL = r"https?://video\.toggle\.sg/(?:en|zh)/(?:series|clips|movies|tv-show)/.+?/(?P<id>[0-9]+)"
 API_USER_PASS_EXPR = r'apiUser:\s*"(?P<user>[^"]+?)".+?apiPass:\s*"(?P<password>[^"]+?)"'
+
+# preferred order of file formats to download
+# highest preference is first
+FILE_PREFERENCES = [(1,"STB","m3u8"),	# generally 720p
+					(2,'ADD','mp4'),	# generally 540p
+					(3,'IPAD','m3u8'),	# generally 540p
+					(4,'IPH','m3u8')]	# generally 360p
 
 # prints usage information only
 def usage():
@@ -105,22 +113,38 @@ def parseurl(url):
 		for ext in ["m3u8", "wvm", "mp4"]:
 			if outputurl.endswith(ext) and outputurl.startswith('http'):
 				urlarray.append(outputurl)
+	
+	if not (autodl):
+	
+		for y in range(1,len(urlarray)+1):
+			print "[%s]: %s" % (y,urlarray[y-1])
 
-	for y in range(1,len(urlarray)+1):
-		print "[%s]: %s" % (y,urlarray[y-1])
+		print "\n[i] Note that for m3u8 and wvm, STB seems to be highest quality, followed by IPAD, then IPH, then ADD."
+		in1 = input('Enter the number to download: ')
 
-	print "\n[i] Note that for m3u8 and wvm, STB seems to be highest quality, followed by IPAD, then IPH, then ADD."
-	in1 = input('Enter the number to download: ')
+		# no error checking here
+		selectedurl = urlarray[in1-1]
+		print "\n[*] Selected %s" % (selectedurl)
 
-	# no error checking here
-	selectedurl = urlarray[in1-1]
-	print "\n[*] Selected %s" % (selectedurl)
+		if (debug):
+			text_file = open("4.selected.txt", "a")
+			text_file.write("{}".format(selectedurl))
+			text_file.write("{}".format("\n"))
+			text_file.close()
+	
+	if (autodl):
+		
+		q1 = Queue.Queue()
+		
+		for priority,qual,format in FILE_PREFERENCES:
+			for url in urlarray:
+				if re.search(qual, url) and re.search(format, url):
+					if (debug):
+						q1.put(url)
+						print "[*] Inserted into queue: %s" % (url)
 
-	if (debug):
-		text_file = open("4.selected.txt", "a")
-		text_file.write("{}".format(selectedurl))
-		text_file.write("{}".format("\n"))
-		text_file.close()
+		selectedurl = q1.get()
+		print "\n[*] Auto-selected URL: %s" % (selectedurl)
 		
 	print "\n[i] Obtaining media name ..."
 	medianame = re.sub(r"\s+", "_", jsondata.get("MediaName", "UNKNOWN"))
@@ -154,10 +178,12 @@ def download(selectedurl, name):
 	if (selectedurl.endswith("m3u8")):
 		print "[i] Crafting ffmpeg command ..."
 		cmd = 'ffmpeg -i ' + selectedurl + " -c copy -bsf:a aac_adtstoasc \"" + name + ".mp4\""
-		print "------------------------------------------------------------"
-		print "[*] Executing the following command: "
-		print cmd
-		raw_input('\nPress <ENTER> to continue')
+		
+		if not autodl:
+			print "------------------------------------------------------------"
+			print "[*] Executing the following command: "
+			print cmd
+			raw_input('\nPress <ENTER> to continue')
 
 		print "[i] Executing ffmpeg command ...\n"
 		dlrtn = os.system(cmd)
